1052258a05f9da260d64d54b674a8029999e6528
kuorthues <lajakurt@gmail.com>
2026-09-11 14:40:59 +0800

    initialized swift proj

---
+++ PACE/PACEApp.swift
@@ -0,0 +1,17 @@
+//
+//  PACEApp.swift
+//  PACE
+//
+//  Created by Mac-LAB on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct PACEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
